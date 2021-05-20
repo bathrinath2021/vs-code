7f5d159f405bbf2c83a93d6e69b9f1c4c677d445
bathrinath2021 <82258050+bathrinath2021@users.noreply.github.com>
2021-05-20 12:03:21 +0530

    Add files via upload
    Backup folder

---
+++ hello.py
@@ -0,0 +1,19 @@
+import math
+print("Hello wolrd")
+lst = [1,2,3,4,5,6,7]#list of numbers
+print(lst)
+st = {5,4,3,2,1}#set 
+print(st)
+rng = list(range(11))#range
+print(rng)
+dict = {"Bathri":"IT","Seethapathy":"ECE","Aaravindh":"MECH"}#Dictionary or Mapping
+print(dict)
+String = "Dictionary"
+
+x = math.sqrt(5)
+print(x)
+print(math.floor(2.9))# it doesn't round of it ignores and print the vlaue 2 and not 3
+print(math.ceil(2.1))# it doesn't round of it ignores and print the vlaue 3 and not 2
+print(math.pow(5,2))
+print(math.pi)# the value of pie = 3.141592653589793
+print(math.e)# the value of epsilon = 2.718281828459045
